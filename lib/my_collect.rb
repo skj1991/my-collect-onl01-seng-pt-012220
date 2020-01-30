5208746ec5a1fb8d7378b151072b4b11bebf604c
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 20:14:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,7 @@
 def my_collect(collection)
   i = 0
-  new_collection = []
   while i < collection.length
-    new_collection << yield (collection[i])
-    i += 1
+    yield (collection[i])
+    i +=1
   end
-  
 end
